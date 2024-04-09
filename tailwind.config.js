4333f2063a0581c83751d761447f4fd2d902b362
object505 <pbeluent@gmail.com>
2024-04-09 13:39:39 +0200

    Initial coming soon

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        main: {
+          text: '#f4f4f4',
+          navyGreen: '#001D22',
+          tealGreen: '#009965',
+        },
       },
     },
   },
